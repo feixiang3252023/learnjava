96c209dff15b665f9985ed7a5da11ccd42463082
feixiang3252023 <feixiang3252023@163.com>
2023-09-29 21:54:10 +0800

    java hello world

---
+++ learn01helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{   
+   
+    // main function 
+    public static void main(String[] args) {
+        Integer a = 1;
+        System.out.println("Hello World!");
+        System.out.println(a.toString());
+    }
+}
+
